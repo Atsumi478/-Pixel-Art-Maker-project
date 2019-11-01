e677c1b32a14a15c0a65486dd43fac4123a2fb35
Atsumi N/A Stepp <52901485+Atsumi478@users.noreply.github.com>
2019-11-01 15:48:28 -0500

    Pixel Art Maker
    Make a grid and add colors on the grid

---
+++ designs.js
@@ -0,0 +1,36 @@
+//宣言する
+const gridColor = document.getElementById('colorPicker');
+const gridTable = document.getElementById('pixelCanvas');
+const sizePicker = document.getElementById('sizePicker');
+//宣言 hight and width
+const height = document.getElementById('inputHeight').value;
+const width = document.getElementById('inputWidth').value;
+makeGrid(height, width);
+
+//preventDefault uses to prevent autmatically refresh
+sizePicker.addEventListener('click', (e) => {
+    e.preventDefault();
+    gridTable.firstElementChild.remove();
+    const height = document.getElementById('inputHeight').value;
+    const width = document.getElementById('inputWidth').value;
+
+    makeGrid(height, width);
+});
+//when size is submitted by user makeGrid()
+//reset color
+function makeGrid(height, width){
+    while(gridTable.length>0){
+      gridTable.reset(makeGrid);
+}
+    for (let i = 0; i < height; i++){
+        let row = gridTable.insertRow(i);
+        for (let c = 0; c < width; c++){
+            let cell = row.insertCell(c);
+            //everything click event happend it adds color
+            cell.addEventListener('click', (e) => {
+                e.preventDefault();
+                cell.style.backgroundColor = gridColor.value;
+            });
+        }
+    }
+}
